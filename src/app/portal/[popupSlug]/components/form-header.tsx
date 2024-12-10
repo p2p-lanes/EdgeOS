2b0b4c034bf293d8b1ad373cf3138252237b2d9a
Chole <alfarojoelf@gmail.com>
2024-12-10 14:20:35 -0300

    [Add] header form

--- src/app/portal/[popupSlug]/components/form-header.tsx
+++ src/app/portal/[popupSlug]/components/form-header.tsx
@@ -2,10 +2,26 @@
 
 import { useCityProvider } from "@/providers/cityProvider"
 import SectionWrapper from "./SectionWrapper"
+import { CalendarDays, MapPin } from "lucide-react"
+import { useEffect } from "react"
+import useWindow from "@/hooks/useWindow"
+import { useState } from "react"
 
 export function FormHeader() {
+  const { isClient } = useWindow()
   const { getCity } = useCityProvider()
   const city = getCity()
+
+  const [calendarDays, setCalendarDays] = useState('')
+
+  useEffect(() => {
+    if(!isClient || !city?.start_date || !city?.end_date) return
+
+    const startDate = new Date(city.start_date).toLocaleDateString('en-EN', {day: 'numeric', month: 'long', year: 'numeric'})
+    const endDate = new Date(city.end_date).toLocaleDateString('en-EN', {day: 'numeric', month: 'long', year: 'numeric'})
+    const calendarDays = startDate + ' - ' + endDate
+    setCalendarDays(calendarDays)
+  }, [city?.start_date, city?.end_date, isClient])
 
   if(!city) return null
 
@@ -19,11 +35,27 @@ export function FormHeader() {
           className="dark:invert"
         />
       </div>
-      <div className="space-y-2">
+      <div className="space-y-3">
         <h1 className="text-3xl font-bold tracking-tight">{city.name} Application</h1>
-        {/* <p className="text-sm text-primary">
-          OUR APPLICATIONS ARE NOW CLOSED. 2025 APPLICATIONS WILL OPEN SOON.
-        </p> */}
+        <p className="text-md text-muted-foreground">
+          {city.tagline}
+        </p>
+        {
+          city.location && (
+            <div className="flex items-center text-sm text-muted-foreground mb-2">
+              <MapPin className="mr-2 h-4 w-4" />
+              {city.location}
+            </div>
+          )
+        }
+        {
+          city.start_date && (
+            <div className="flex items-center text-sm text-muted-foreground mb-4">
+              <CalendarDays className="mr-2 h-4 w-4" />
+              {calendarDays}
+            </div>
+          )
+        }
       </div>
     </SectionWrapper>
   )
