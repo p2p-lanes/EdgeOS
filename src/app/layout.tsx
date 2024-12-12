8605ddc57ae58a7481c9da697ac5c37f221e8a48
tule <aromeoes@gmail.com>
2024-12-12 09:41:48 -0300

    feat: open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,20 @@ import { Toaster } from "sonner";
 import icon from '../../public/EdgeCityIcon.png'
 
 export const metadata: Metadata = {
-  title: "Resident Portal",
-  description: "Your gateway to EdgeCity",
+  title: "Edge Portal",
+  description: "Your gateway to Edge Esmeralda",
   icons: {
     icon: icon.src,
+  },
+  openGraph: {
+    title: "Edge Portal",
+    description: "Your gateway to Edge Esmeralda",
+    images: [{
+      url: "https://cdn.prod.website-files.com/67475a01312f8d8225a6b46e/675ad7fdc9bcb89ffc5e4d5c_EE25-header-img-2-min%202.jpg",
+      width: 1200,
+      height: 630,
+      alt: 'Edge Portal'
+    }]
   }
 };
 
