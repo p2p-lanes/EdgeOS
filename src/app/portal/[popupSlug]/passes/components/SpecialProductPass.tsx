51e8d9e18c72664a7f18ebfb809d5d5444b5e725
Chole <alfarojoelf@gmail.com>
2025-01-14 10:45:01 -0300

    [Add] patreon style

--- src/app/portal/[popupSlug]/passes/components/SpecialProductPass.tsx
+++ src/app/portal/[popupSlug]/passes/components/SpecialProductPass.tsx
@@ -1,6 +1,6 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 import { ProductsPass } from "@/types/Products";
-import { Check, Info, Plus } from 'lucide-react'
+import { Check, Crown, Info, Plus } from 'lucide-react'
 
 interface SpecialProductPassProps {
   product: ProductsPass;
@@ -20,7 +20,7 @@ export default function SpecialProductPass({ product, disabled, selected, onClic
         ${selected || disabled ? 'border-2 border-[#16B74A] bg-[#D5F7CC]' : 'cursor-pointer border-2 border-gray-200 bg-transparent hover:bg-gray-100'}
       `}
     >
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 py-2">
         {
           selected || disabled ? (
             <Check className="w-4 h-4" color="#005F3A"/>
@@ -28,13 +28,10 @@ export default function SpecialProductPass({ product, disabled, selected, onClic
             <Plus className="w-4 h-4" />
           )
         }
-        <span className={`font-medium ${selected ? 'text-[#005F3A]' : ''} flex items-center gap-2`}>
+        <span className={`font-semibold ${selected ? 'text-[#005F3A]' : ''} flex items-center gap-2`}>
+          <Crown className="w-5 h-5 text-orange-500" />
           {product.name}
-          {
-            product.category === 'patreon' && (
-              <TooltipPatreon/>
-            )
-          }
+          <TooltipPatreon/>
         </span>
       </div>
       
