0997d320c740440b2406aa371bb4e2d9905d3899
Chole <alfarojoelf@gmail.com>
2025-01-16 13:09:02 -0300

    [Fix] discount assign

--- src/app/portal/[popupSlug]/passes/components/Sidebar/Passes/Components/BannerDiscount.tsx
+++ src/app/portal/[popupSlug]/passes/components/Sidebar/Passes/Components/BannerDiscount.tsx
@@ -7,16 +7,17 @@ const BannerDiscount = ({isPatreon, application, products}: {isPatreon: boolean,
 
   const {discount, label} = useMemo(() => {
     if (isPatreon) return {discount: 100, label: 'discount applied'}
-
-    if(application.discount_assigned === 0 && !productCompare) return {discount: 0, label: ''}
-
+    
+    if(!application.discount_assigned && !productCompare.compare_price) return {discount: 0, label: ''}
+    
     if(application.discount_assigned) return {discount: application.discount_assigned, label: 'discount applied'}
-
+    
     const discount = 100 - ((productCompare.price ?? 0) / (productCompare.compare_price ?? 0) * 100)
     return {discount: Math.round(discount), label: 'early bird discount'}
     
   }, [isPatreon, application, productCompare])
-
+  
+  console.log('productCompare', application.discount_assigned, productCompare.compare_price)
   if(discount === 0 || !productCompare) return null
 
   return (
