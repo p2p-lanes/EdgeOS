16909443b23da83c7d24be19aa8e33b2cf1276d7
Chole <alfarojoelf@gmail.com>
2025-05-19 12:02:22 -0300

    [Add] bhutan

--- src/app/portal/[popupSlug]/application/components/CardVideo.tsx
+++ src/app/portal/[popupSlug]/application/components/CardVideo.tsx
@@ -10,6 +10,8 @@ const CardVideo = ({ videoUrl, setVideoUrl }: { videoUrl: string, setVideoUrl: (
   const { getCity } = useCityProvider()
   const city = getCity()
 
+  const isBhutan = city?.slug?.includes('bhutan')
+
   return (
     <Card className="px-6 mt-6 bg-slate-50 border-dashed border-2 border-slate-400">
         <div className="grid gap-4 sm:grid-cols-1 my-4">
@@ -22,12 +24,30 @@ const CardVideo = ({ videoUrl, setVideoUrl }: { videoUrl: string, setVideoUrl: (
               </div>
               <Tooltip>
                 <Label htmlFor="video_url" className="text-black"> Please record a 1-2 minute video sharing your quick response to the {' '}
-                  <TooltipTrigger asChild>
-                    <span className="font-bold underline">following questions</span>
-                  </TooltipTrigger>
+                  {
+                    !isBhutan ? (
+                      <TooltipTrigger asChild>
+                        <span className="font-bold underline">following questions</span>
+                      </TooltipTrigger>
+                    ) : (
+                     'following questions'
+                    )
+                  }
                   {' '}
                   (you don’t have to fill them in below if you add the video)
                 </Label>
+                {
+                  isBhutan && (
+                    <Label className="text-black">
+                      <p className="text-sm text-gray-700 mt-1">
+                        - What are your goals for {city?.name} and why do you want to join?
+                      </p>
+                      <p className="text-sm text-gray-700 mb-1">
+                        - What is something you could contribute? A workshop, a talk, an area of expertise. Get creative!
+                      </p>
+                    </Label>
+                  )
+                }
                 <TooltipContent className="bg-white shadow-md border border-gray-200 max-w-sm">
                   <p className="text-sm text-gray-700 mt-1">
                     - What are your goals for {city?.name} and why do you want to join?
