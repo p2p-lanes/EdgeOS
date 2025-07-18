cd7eba2f638d9a1e10312148d7086a63b21dfff9
Chole <alfarojoelf@gmail.com>
2025-07-18 11:16:05 -0300

    [add] collectibles

--- src/app/portal/poaps/components/PoapsList.tsx
+++ src/app/portal/poaps/components/PoapsList.tsx
@@ -15,21 +15,21 @@ const PoapsList = () => {
     );
   }
 
-  if (!poaps || poaps.length === 0) {
+  // if (!poaps || poaps.length === 0) {
     return (
-      <div className="flex justify-center items-center h-40">
-        <p className="text-gray-500">No POAPs available right now.</p>
+      <div className="flex justify-center items-center h-48">
+        <p className="text-gray-500 text-md font-medium">No collectibles available right now.</p>
       </div>
     );
-  }
+  // }
 
-  return (
-    <div className="flex flex-wrap gap-6 mt-6 mx-2">
-      {poaps.map((poap: PoapProps, index: number) => (
-        <Poap key={poap.id || poap._id || `poap-${index}`} poap={poap} />
-      ))}
-    </div>
-  );
+  // return (
+  //   <div className="flex flex-wrap gap-6 mt-6 mx-2">
+  //     {poaps.map((poap: PoapProps, index: number) => (
+  //       <Poap key={poap.id || poap._id || `poap-${index}`} poap={poap} />
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default PoapsList; 
