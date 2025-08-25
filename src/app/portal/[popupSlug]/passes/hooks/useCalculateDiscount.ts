21f64159a05772e9c7d66f54adeafb9a4dbc2377
Chole <alfarojoelf@gmail.com>
2025-08-25 10:14:17 -0300

    [fix] banner

--- src/app/portal/[popupSlug]/passes/hooks/useCalculateDiscount.ts
+++ src/app/portal/[popupSlug]/passes/hooks/useCalculateDiscount.ts
@@ -15,15 +15,15 @@ const useCalculateDiscount = (isPatreon: boolean, products: ProductsProps[]) => 
   const {discount, label, isEarlyBird} = useMemo(() => {
     if (isPatreon) return {discount: 100, label: 'As a Patron, you are directly supporting the ecosystem.'}
     
-    if(!application || !application.discount_assigned && !productCompare.compare_price && !discountApplied.discount_value) return {discount: 0, label: ''}
+    if(!application) return {discount: 0, label: ''}
     
     if(discountApplied.discount_value){
       if(application.discount_assigned) return {discount: discountApplied.discount_value, label: `You've been awarded a special ${discountApplied.discount_value}% discount. Enjoy!`}
       return {discount: discountApplied.discount_value, label: `You've unlocked an extra ${discountApplied.discount_value}% off with your code. Enjoy!`}
     }
-
+    
     if(city?.ticketing_banner_description) return {discount: 0.01, label: city.ticketing_banner_description}
-
+    
     const discount = 100 - ((productCompare.price ?? 0) / (productCompare.compare_price ?? 0) * 100)
     
     return {discount: Math.round(discount), label: `${Math.round(discount)}% early bird discount`, isEarlyBird: true}
